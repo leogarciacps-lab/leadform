41569560924bc876931a7acebc457c0efa8bffac
leogarciacps-lab <leo.garciacps@gmail.com>
2025-11-04 16:15:32 -0600

    use Render port from environment variable

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
